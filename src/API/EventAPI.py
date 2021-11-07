fcb3a9c9469d46af59a2632e35239b0577b31d47
Nazarii Hryhorash <grigorashnazar@gmail.com>
2021-11-07 14:58:41 +0200

    API have been implemented

---
+++ src/API/EventAPI.py
@@ -0,0 +1,88 @@
+from flask_restful import Resource
+from flask import jsonify, request, Blueprint
+from src.database.tables import *
+from flask_jwt_extended import jwt_required, get_jwt_identity
+
+evquery = Blueprint('evquery', __name__)
+
+
+class EventAPI(Resource):
+    def get(self):
+        try:
+            events = Event.query.all()
+            serialised = []
+            for event in events:
+                serialised.append({
+                    'id': event.id,
+                    'name': event.name,
+                    'description': event.description,
+                    'location': event.location,
+                    'date': event.date
+                })
+            return jsonify(serialised), 200
+        except Exception:
+            return 'Unsuccessful operation', 403
+
+    @jwt_required()
+    def post(self):
+        try:
+            user_id = get_jwt_identity()
+            user = User.query.filter(User.id == user_id).first()
+            if not user.permissions == 'admin':
+                return 'Do not have permissions', 401
+
+            new_event = Event(**request.json)
+            session.add(new_event)
+            session.commit()
+            session.close()
+            return 'Successful operation', 200
+        except Exception:
+            return 'Unsuccessful operation', 403
+
+
+class EventParamAPI(Resource):
+    @jwt_required()
+    def put(self, event_id):
+        try:
+            user_id = get_jwt_identity()
+            user = User.query.filter(User.id == user_id).first()
+            if not user.permissions == 'admin':
+                return 'Do not have permissions', 401
+
+            params = request.json
+            item = Event.query.filter(Event.id == event_id).first()
+            if not item:
+                return "Invalid ID supplied", 402
+
+            for key, value in params.items():
+                setattr(item, key, value)
+            session.commit()
+            session.close()
+
+            return 'Successful operation', 200
+        except Exception:
+            return 'Unsuccessful operation', 403
+
+    @jwt_required()
+    def delete(self, event_id):
+        try:
+            user_id = get_jwt_identity()
+            user = User.query.filter(User.id == user_id).first()
+            if not user.permissions == 'admin':
+                return 'Do not have permissions', 401
+
+            item = Event.query.filter(Event.id == event_id).first()
+            if not item:
+                return 'Invalid ID supplied', 402
+
+            session.delete(item)
+            session.commit()
+            session.close()
+
+            return 'Successful operation', 200
+        except Exception:
+            return 'Unsuccessful operation', 403
+
+
+evquery.add_url_rule('/event', view_func=EventAPI.as_view("eventAPI"))
+evquery.add_url_rule('/event/<int:event_id>', view_func=EventParamAPI.as_view("eventParamAPI"))
--- src/database/tables.py
+++ src/database/tables.py
@@ -1,11 +1,18 @@
 from sqlalchemy import Column, ForeignKey, Integer, String, DateTime, DECIMAL
-from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import relationship
 from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker, scoped_session
+from sqlalchemy.ext.declarative import declarative_base
+from flask_jwt_extended import create_access_token
+from datetime import timedelta
+from passlib.hash import bcrypt
 
 engine = create_engine("mysql+mysqlconnector://root:myrootpassword@localhost/mydb", echo=True)
 
+session = scoped_session(sessionmaker(autocommit=False, autoflush=False, bind=engine))
+
 Base = declarative_base()
+Base.query = session.query_property()
 
 
 class User(Base):
@@ -15,9 +22,29 @@ class User(Base):
     nickname = Column(String(250), nullable=False)
     name = Column(String(250), nullable=False)
     surname = Column(String(250), nullable=False)
-    email = Column(String(250), nullable=False)
+    email = Column(String(250), nullable=False, unique=True)
     password = Column(String(250), nullable=False)
     permissions = Column(String(250), nullable=False)
+
+    def __init__(self, **kwargs):
+        self.name = kwargs.get('name')
+        self.surname = kwargs.get('surname')
+        self.nickname = kwargs.get('nickname')
+        self.email = kwargs.get('email')
+        self.password = bcrypt.hash(kwargs.get('password'))
+        self.permissions = 'user'
+
+    def get_token(self, expire_time=1):
+        expire_delta = timedelta(expire_time)
+        token = create_access_token(identity=self.id, expires_delta=expire_delta)
+        return token
+
+    @classmethod
+    def authentication(cls, email, password):
+        user = cls.query.filter(cls.email == email).one()
+        if not bcrypt.verify(password, user.password):
+            raise Exception('No user')
+        return user
 
     def __repr__(self):
         return f"< User: {str(self.id)}, {self.nickname}, {self.name}, {self.surname}, " \
